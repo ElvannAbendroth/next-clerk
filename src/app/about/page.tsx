8a2bebee2d3ff0a3180a502d9297fd24cc03430e
Elvann <elvann.abendroth@gmail.com>
2024-08-28 10:39:32 +0300

    ⚙️clerk dev

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+interface AboutPageProps {}
+
+export default function AboutPage({}) {
+  return (
+    <div className="flex flex-col py-12 px-4 max-w-7xl mx-auto">
+      <h1 className="text-2xl uppercase font-bold">About</h1>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -19,8 +21,33 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+          {' '}
+          <nav className="py-3 px-4 flex items-center gap-4 justify-between shadow-md sticky top-0">
+            <div id="Logo" className="flex gap-3 items-center">
+              <div className="rounded-full bg-accent size-8"></div>
+              <p className="uppercase font-bold">Website Title</p>
+            </div>
+            <div id="nav-links" className="flex gap-4 items-center">
+              <Link href="/" className="hover:underline hover:text-foreground/80 transition-all">
+                Home
+              </Link>
+              <Link href="/about" className="hover:underline hover:text-foreground/80 transition-all">
+                About
+              </Link>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
